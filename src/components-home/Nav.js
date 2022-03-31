9b437b60e9a914eeeb2ef73501699f41dd4aa585
sglover98 <88252222+sglover98@users.noreply.github.com>
2022-03-30 21:11:00 -0400

    master

--- src/components-home/Nav.js
+++ src/components-home/Nav.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Skull1 from "../assets/cyber-attack (1).png"
 import { Nav, Navbar, Container,Col } from 'react-bootstrap';
 
 export default function Navbars() {
@@ -9,14 +10,14 @@ export default function Navbars() {
       <div className='Nav' class='fixed-top'>
         <Navbar bg="dark" expand="lg" mw-100  color='text-light'>
         <Container className='Navbars'>
-          <Navbar.Brand href="#home" >React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Brand href="#home" > <img src={Skull1} className="Skull1" alt=""/></Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" className='burger' />
           <Navbar.Collapse id="basic-navbar-nav ">
             <Nav className="me-auto">
               <Nav.Link href="/home" >Home</Nav.Link>
               <Nav.Link href="/about" >About</Nav.Link>
               <Nav.Link href="/projects" >Projects</Nav.Link>
-              <Nav.Link href="www.google.com" >Resume</Nav.Link>
+              <Nav.Link href="https://drive.google.com/file/d/1lc1Mwn6JLJv7z3Zd5kTCrKYBRlv_Tsv7/view?usp=sharing" >Resume</Nav.Link>
 
 
             </Nav>
